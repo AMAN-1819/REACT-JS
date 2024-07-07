5fe00f6e2b1f4316bd7b27e2d90db0973e546fda
AMAN-1819 <amanojha1908@gmail.com>
2024-07-07 10:57:05 +0530

    minicontext

---
+++ 08miniContext/src/App.jsx
@@ -0,0 +1,14 @@
+import UserContextProvider from "./context/UserContextProvider"
+
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+    <h1>hello</h1>
+    </UserContextProvider>
+  )
+}
+
+export default App
---
+++ 08miniContext/src/components/Login.jsx
@@ -0,0 +1,13 @@
+import React,{useState,useContext} from 'react'
+import UserContext from '../context/UserContext'
+function Login() {
+  return (
+    <div>
+     <h2>Login</h2>   
+
+      
+    </div>
+  )
+}
+
+export default Login
